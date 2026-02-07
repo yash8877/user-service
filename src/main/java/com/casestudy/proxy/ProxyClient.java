6b1a559df4c4f5720167dc50718f1cc972a0b204
Yash Raj <yash524767@gmail.com>
2026-02-07 22:58:31 +0530

    new changes

--- src/main/java/com/casestudy/proxy/ProxyClient.java
+++ src/main/java/com/casestudy/proxy/ProxyClient.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service")
+@FeignClient(name = "auth-service",url = "${auth.service.url}")
 public interface ProxyClient {
     @GetMapping("/auth/get-roles/{username}")
     String getRolesById(@PathVariable String username);
 
     @DeleteMapping("/auth/delete/id/{userId}")
-    public String deleteUser(@PathVariable Long userId);
+    void deleteUser(@PathVariable Long userId);
 }
